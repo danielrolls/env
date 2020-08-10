2e2f2121060f9dd48c9b4440fd474d6cbb6234f4
Daniel Rolls <daniel.rolls.27@googlemail.com>
2020-08-10 22:46:37 +0100

    Add nix home-manager base

---
+++ home-manager/home.nix
@@ -0,0 +1,48 @@
+{
+  programs.taskwarrior = {
+    enable = true;
+    colorTheme = "light-256";
+    config = {
+      urgency.uda.priority = {
+        L.coefficient = -1.0;
+        M.coefficient = 5; # + 1.1
+        H.coefficient = 10; # + 4
+      };
+      uda.details = {
+        type = "string";
+        label = "Details";
+      };
+      report = {
+        next.filter = "-chase status:pending limit:page";
+        chase = {
+          description = "Tasks to chase up";
+          columns = "id,start.age,entry.age,depends,priority,project,recur,scheduled.countdown,due.relative,until.remaining,description,urgency";
+          filter = "+chase status:pending limit:page";
+          labels = "ID,Active,Age,Deps,P,Project,Recur,S,Due,Until,Description,Urg";
+          sort = "urgency-";
+        };
+      };
+    };
+  };
+
+  home.packages = [
+    #pkgs.direnv
+    # pkgs.bash-completion
+  ];
+
+
+  programs.direnv = {
+    enable = true;
+  };
+
+  home.file = {
+    ".inputrc".text = "set editing-mode vi";
+
+    ".haskeline".text = ''
+      maxHistorySize: Nothing
+      historyDuplicates: IgnoreConsecutive
+      editMode: Vi
+    '';
+  };
+
+}
