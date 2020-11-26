92c2f7279ffa70b74a4a8645bcef6b091dc93e6d
Daniel Rolls <daniel.rolls.27@googlemail.com>
2020-11-26 21:38:32 +0000

    Made home config importable via std mechanism

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -1,64 +1,85 @@
+{ config, lib, pkgs, ... }:
+
 {
-  programs.taskwarrior = {
-    enable = true;
-    colorTheme = "light-256";
-    config = {
-      urgency.uda.priority = {
-        L.coefficient = -1.0;
-        M.coefficient = 5; # + 1.1
-        H.coefficient = 10; # + 4
+  imports = [ <home-manager/nixos> ];
+
+  home-manager.users.dan = { pkgs, ... }: {
+
+    programs = {
+
+      vim = {
+        enable = true;
       };
-      uda.details = {
-        type = "string";
-        label = "Details";
+
+      bash.enable = true;
+
+      git = {
+        enable = true;
+        userName = "Daniel Rolls";
+        userEmail = "daniel.rolls.27@googlemail.com";
       };
-      report = {
-        next.filter = "-chase status:pending limit:page";
-        chase = {
-          description = "Tasks to chase up";
-          columns = "id,start.age,entry.age,depends,priority,project,recur,scheduled.countdown,due.relative,until.remaining,description,urgency";
-          filter = "+chase status:pending limit:page";
-          labels = "ID,Active,Age,Deps,P,Project,Recur,S,Due,Until,Description,Urg";
-          sort = "urgency-";
+
+      taskwarrior = {
+        enable = true;
+        colorTheme = "light-256";
+        config = {
+          urgency.uda.priority = {
+            L.coefficient = -1.0;
+            M.coefficient = 5; # + 1.1
+            H.coefficient = 10; # + 4
+          };
+          uda.details = {
+            type = "string";
+            label = "Details";
+          };
+          report = {
+            next.filter = "-chase status:pending limit:page";
+            chase = {
+              description = "Tasks to chase up";
+              columns = "id,start.age,entry.age,depends,priority,project,recur,scheduled.countdown,due.relative,until.remaining,description,urgency";
+              filter = "+chase status:pending limit:page";
+              labels = "ID,Active,Age,Deps,P,Project,Recur,S,Due,Until,Description,Urg";
+              sort = "urgency-";
+            };
+          };
         };
       };
     };
-  };
 
-  home.packages = [
-    #pkgs.direnv
-    # pkgs.bash-completion
-  ];
+    home.packages = with pkgs; [
+      stack
+    ];
 
-  
-  home.sessionVariables = {
-    EDITOR = "vim";
-  };
+    
+    home.sessionVariables = {
+      EDITOR = "vim";
+    };
 
-  programs.bash.shellAliases = {
-    tasks = "task sync ; task";
-  };
+    programs.bash.shellAliases = {
+      tasks = "task sync ; task";
+    };
 
 
-  programs.direnv.enable = true;
+    programs.direnv.enable = true;
 
-  home.file = {
-    ".inputrc".text = "set editing-mode vi";
+    home.file = {
+      ".inputrc".text = "set editing-mode vi";
 
-    ".haskeline".text = ''
-      maxHistorySize: Nothing
-      historyDuplicates: IgnoreConsecutive
-      editMode: Vi
-    '';
+      ".haskeline".text = ''
+        maxHistorySize: Nothing
+        historyDuplicates: IgnoreConsecutive
+        editMode: Vi
+      '';
 
-    ".stack/config.yaml".text = ''
-      templates:
-        params:
-          author-email: daniel.rolls.27@googlemail.com
-          author-name: Daniel Rolls
-          github-username: danielrolls
-    '';
+      ".stack/config.yaml".text = ''
+        templates:
+          params:
+            author-email: daniel.rolls.27@googlemail.com
+            author-name: Daniel Rolls
+            github-username: danielrolls
+      '';
 
+    };
 
   };
 
