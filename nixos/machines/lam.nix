12d19e5e54f1c606958151dd0caf4b52b02fdec7
Daniel Rolls <daniel.rolls.27@googlemail.com>
2021-01-31 13:40:57 +0000

    Added lam disk config

--- nixos/machines/lam.nix
+++ nixos/machines/lam.nix
@@ -2,5 +2,11 @@
   networking = {
     hostName = "lam"; 
   };
+
+  fileSystems."/data" = {
+    device = "/dev/disk/by-uuid/c6f074cc-ba18-4c58-911e-376c24127eff";
+    options = ["nofail"];
+  };
+
 }
 
