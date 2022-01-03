87c01b0d6ee34fb30b93257f0f08f80f460cd271
Daniel Rolls <daniel.rolls.27@googlemail.com>
2022-01-03 13:36:57 +0000

    Invoke shell prompt with git branch where available

--- nixos/dev.nix
+++ nixos/dev.nix
@@ -4,6 +4,27 @@ userName:
 {
   imports = [ <home-manager/nixos> ];
 
+  programs.bash.promptInit = ''
+       # Provide a nice prompt if the terminal supports it.
+       if [ "$TERM" != "dumb" ] || [ -n "$INSIDE_EMACS" ]; then
+         PROMPT_COLOR="1;31m"
+         ((UID)) && PROMPT_COLOR="1;32m"
+         red='\[\e[0;31m\]'
+         green='\[\e[0;32m\]'
+         black='\[\e[0;0m\]'
+         if [ -n "$INSIDE_EMACS" ] || [ "$TERM" = "eterm" ] || [ "$TERM" = "eterm-color" ]; then
+           # Emacs term mode doesn't support xterm title escape sequence (\e]0;)
+           PS1="\n\[\033[$PROMPT_COLOR\][\u@\h:\w]\\$\[\033[0m\] "
+         else
+	   . ${pkgs.git}/share/bash-completion/completions/git-prompt.sh
+           PS1="\n\[\033[$PROMPT_COLOR\][\[\e]0;\u@\h: \w\a\]\u@\h:\w"'$(__git_ps1 ":%s")'"]\\$\[\033[0m\] "
+         fi
+         if test "$TERM" = "xterm"; then
+           PS1="\[\033]2;\h:\u:\w\007\]$PS1"
+         fi
+       fi
+  '';
+
   home-manager.users."${userName}" = { pkgs, ... }: {
 
     programs = {
