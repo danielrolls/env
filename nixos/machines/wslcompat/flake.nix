cd147aefd67a5c327ff808b88903f3ed4138ba38
Daniel Rolls <daniel.rolls.27@googlemail.com>
2022-01-12 22:27:33 +0000

    Added wslcompat flake

---
+++ nixos/machines/wslcompat/flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "Nixos Flake for wslcompat";
+
+  inputs = {
+    nixos.url = "github:nixos/nixpkgs/nixos-21.11";
+    home-manager.url = "github:nix-community/home-manager/release-21.11";
+    home-manager.inputs.nixpkgs.follows = "nixos";
+  };
+
+  outputs = { self, nixos, home-manager }: {
+
+    nixosConfigurations."wslcompat" = nixos.lib.nixosSystem {
+      system = "x86_64-linux";
+
+      modules = [
+        home-manager.nixosModules.home-manager {
+          home-manager.useGlobalPkgs = true;
+	}
+        ./wslcompat-master.nix
+      ];
+    };
+  };
+}
---
+++ nixos/machines/wslcompat/wslcompat-master.nix
@@ -0,0 +1,56 @@
+{ lib, pkgs, config, modulesPath, ... }:
+
+with lib;
+let
+  defaultUser = "dro25";
+  syschdemd = import ./syschdemd.nix { inherit lib pkgs config defaultUser; };
+in
+{
+  imports = [
+    "${modulesPath}/profiles/minimal.nix"
+    ((import ../../homely.nix) defaultUser)
+    ((import ../../dev.nix) defaultUser)
+  ];
+
+  home-manager.users."dro25".programs = {
+    taskwarrior.dataLocation = "/mnt/c/Users/dro25/OneDrive\ -\ Sky/scarlethomeshare/taskwarrior";
+  };
+
+  virtualisation.docker.enable = true;
+
+  environment.noXlibs = lib.mkForce false;
+
+  # WSL is closer to a container than anything else
+  boot.isContainer = true;
+
+  environment.etc.hosts.enable = false;
+  environment.etc."resolv.conf".enable = false;
+
+  networking.dhcpcd.enable = false;
+
+  users.users.${defaultUser} = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" "docker" ];
+  };
+
+  users.users.root = {
+    shell = "${syschdemd}/bin/syschdemd";
+    # Otherwise WSL fails to login as root with "initgroups failed 5"
+    extraGroups = [ "root" ];
+  };
+
+  security.sudo.wheelNeedsPassword = false;
+
+  # Disable systemd units that don't make sense on WSL
+  systemd.services."serial-getty@ttyS0".enable = false;
+  systemd.services."serial-getty@hvc0".enable = false;
+  systemd.services."getty@tty1".enable = false;
+  systemd.services."autovt@".enable = false;
+
+  systemd.services.firewall.enable = false;
+  systemd.services.systemd-resolved.enable = false;
+  systemd.services.systemd-udevd.enable = false;
+
+  # Don't allow emergency mode, because we don't have a console.
+  systemd.enableEmergencyMode = false;
+}
