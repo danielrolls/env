7a11ccf72a985bff14994730b3a732761a64d088
Daniel Rolls <daniel.rolls.27@googlemail.com>
2022-03-25 22:16:03 +0000

    Remove redndant config from devvm flake

--- machines/devvm/flake.nix
+++ machines/devvm/flake.nix
@@ -9,18 +9,6 @@
 
   outputs = { self, nixos, home-manager }: {
 
-    nixosConfigurations."lam" = nixos.lib.nixosSystem {
-      system = "x86_64-linux";
-
-      modules = [
-        home-manager.nixosModules.home-manager {
-          home-manager.useGlobalPkgs = true;
-	}
-        ./devvm-master.nix
-	(import ./testvm.nix "dan")
-      ];
-    };
-
     # nix build '.#devvm'
     devvm = let system = "x86_64-linux";
     in import "${nixos}/nixos/lib/make-disk-image.nix" {
