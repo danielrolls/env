f1cecf6affb9adcb871145ba101bfeee84422e8e
Daniel Rolls <daniel.rolls.27@googlemail.com>
2022-04-23 11:43:32 +0100

    Remove nvd: instead run nix store diff-closures
    nvd: 0.1.2 → ∅, [32;1m-27.0 KiB[0m

--- homely.nix
+++ homely.nix
@@ -10,7 +10,6 @@ userName:
     (pkgs.writeShellScriptBin "nixFlakes" ''
       exec ${pkgs.nixFlakes}/bin/nix --experimental-features "nix-command flakes" "$@"
     '')
-    nvd
   ];
 
   users.users."${userName}" = {
