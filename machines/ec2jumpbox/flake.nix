4f3f6622ae99e66d884ed27007a26eb2890d42b0
Daniel Rolls <daniel.rolls.27@googlemail.com>
2022-10-23 19:43:32 +0100

    AWS jumpbox machine

---
+++ machines/ec2jumpbox/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "AWS Jumpbox";
+
+  inputs = {
+    nixos.url = "github:nixos/nixpkgs/nixos-21.11";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-21.11";
+      inputs.nixpkgs.follows = "nixos";
+    };
+  };
+
+  outputs = { self, nixos, home-manager }: {
+    nixosConfigurations."ec2jumpbox" = nixos.lib.nixosSystem {
+      system = "x86_64-linux";
+
+      modules = [
+        home-manager.nixosModules.home-manager {
+          home-manager.useGlobalPkgs = true;
+	}
+	"${nixos}/nixos/modules/virtualisation/amazon-image.nix"
+        ((import ../../homely.nix) "dan")
+      ];
+      
+    };
+  };
+}
